45193d63f7ce098b2805420410bbc0940ff04337
Dmytro Oparivsky <kristobalkomon@gmail.com>
2022-06-07 21:17:49 +0300

    mercedes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+   $('.design__slider').slick({
+      dots: false,
+      slidesToShow: 4,
+      variableWidth: true,
+      prevArrow: '<img class="arrow-west" src="img/west.svg" alt="">',
+      nextArrow: '<img class="arrow-east" src="img/east.svg" alt="">',
+
+   });
+});
